6474a26ca6af229646f190bbc6126dfb2b0b7e36
RNA <RNA0@protonmail.com>
2021-08-28 19:16:20 +0300

    git init

--- ControllerTestNX/MainWindow.xaml.cs
+++ ControllerTestNX/MainWindow.xaml.cs
@@ -1,87 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace ControllerTestNX
 {
-    public partial class MainWindow
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        private const int TIMES_PRESS_TO_EXIT = 5;
-        private const int MAX_COUNT = 40;
-        private static string OnExitButtonKey = "";
-        private static int OnExitButtonCount;
-
         public MainWindow()
         {
             InitializeComponent();
-            AddTimesToExitComponentText();
-        }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            var currentKey = e.Key.ToString();
-            ExitCheckXTimes(currentKey);
-            CreateKeyTextComponent(currentKey);
-            RemoveKeyTextComponentLeftovers();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ExitApplication();
-        }
-
-        private void AddTimesToExitComponentText()
-        {
-            PressTimesToExitText.Text = $"Press any button {TIMES_PRESS_TO_EXIT} times to end the test.";
-        }
-
-        private void CreateKeyTextComponent(string currentKey)
-        {
-            Border keyBorder = new()
-            {
-                BorderBrush = Brushes.Black,
-                BorderThickness = new Thickness(1),
-                Margin = new Thickness(0, 0, 5, 0),
-                Padding = new Thickness(2, 0, 2, 0)
-            };
-            TextBlock singleButtonPress = new()
-            {
-                Text = currentKey,
-                FontSize = 20,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                Margin = new Thickness(5)
-            };
-
-            keyBorder.Child = singleButtonPress;
-            ButtonsStack.Children.Insert(0, keyBorder);
-        }
-
-        private void RemoveKeyTextComponentLeftovers()
-        {
-            if (ButtonsStack.Children.Count == MAX_COUNT)
-            {
-                ButtonsStack.Children.RemoveAt(MAX_COUNT - 1);
-            }
-        }
-
-        private static void ExitCheckXTimes(string currentKey)
-        {
-            if (currentKey != OnExitButtonKey)
-            {
-                OnExitButtonKey = currentKey;
-                OnExitButtonCount = 1;
-            }
-            else if (++OnExitButtonCount == TIMES_PRESS_TO_EXIT)
-            {
-                ExitApplication();
-            }
-        }
-
-        private static void ExitApplication()
-        {
-            Environment.Exit(0);
         }
     }
 }
