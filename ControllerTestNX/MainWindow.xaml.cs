234f72b6fac41c2831238581edfaea964975d1bb
RNA <RNA0@protonmail.com>
2021-09-03 17:08:18 +0300

    The program now supports all key types from key down input.

--- ControllerTestNX/MainWindow.xaml.cs
+++ ControllerTestNX/MainWindow.xaml.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ControllerTestNX
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
+
+		private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+            Border keyBborder = new()
+            {
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1),
+                Margin = new Thickness(0, 0, 5, 0),
+                Padding = new Thickness(5)
+            };
+            TextBlock SingleButtonPress = new()
+			{
+				Text = e.Key.ToString(),
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(5)
+            };
+            keyBborder.Child = SingleButtonPress;
+            ButtonsStack.Children.Add(keyBborder);
+		}
+	}
 }
