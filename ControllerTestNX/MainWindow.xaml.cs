e7fbcedfe0369fd22fc4b5a4d4b2e8e39c9267fe
RNA <RNA0@protonmail.com>
2021-09-15 13:31:35 +0300

    add 5 times to exit and separated long code to functions

--- ControllerTestNX/MainWindow.xaml.cs
+++ ControllerTestNX/MainWindow.xaml.cs
@@ -8,12 +8,36 @@ namespace ControllerTestNX
 {
     public partial class MainWindow
     {
+        private const int TIMES_PRESS_TO_EXIT = 5;
+        private const int MAX_COUNT = 40;
+        private static string OnExitButtonKey = "";
+        private static int OnExitButtonCount;
+
         public MainWindow()
         {
             InitializeComponent();
+            AddTimesToExitComponentText();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            var currentKey = e.Key.ToString();
+            ExitCheckXTimes(currentKey);
+            CreateKeyTextComponent(currentKey);
+            RemoveKeyTextComponentLeftovers();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExitApplication();
+        }
+
+        private void AddTimesToExitComponentText()
+        {
+            PressTimesToExitText.Text = $"Press any button {TIMES_PRESS_TO_EXIT} times to end the test.";
+        }
+        
+        private void CreateKeyTextComponent(string currentKey)
         {
             Border keyBorder = new()
             {
@@ -24,21 +48,38 @@ namespace ControllerTestNX
             };
             TextBlock singleButtonPress = new()
             {
-                Text = e.Key.ToString(),
+                Text = currentKey,
                 FontSize = 20,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 Margin = new Thickness(5)
             };
+
             keyBorder.Child = singleButtonPress;
             ButtonsStack.Children.Insert(0, keyBorder);
-            const int maxCount = 40;
-            if (ButtonsStack.Children.Count == maxCount)
+        }
+
+        private void RemoveKeyTextComponentLeftovers()
+        {
+            if (ButtonsStack.Children.Count == MAX_COUNT)
             {
-                ButtonsStack.Children.RemoveAt(maxCount - 1);
+                ButtonsStack.Children.RemoveAt(MAX_COUNT - 1);
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private static void ExitCheckXTimes(string currentKey)
+        {
+            if (currentKey != OnExitButtonKey)
+            {
+                OnExitButtonKey = currentKey;
+                OnExitButtonCount = 1;
+            }
+            else if (++OnExitButtonCount == TIMES_PRESS_TO_EXIT)
+            {
+                ExitApplication();
+            }
+        }
+
+        private static void ExitApplication()
         {
             Environment.Exit(0);
         }
